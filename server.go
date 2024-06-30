5c25531f17c1c401928b7dd0f8851d808ba2de61
EspiraMarvin <espiraMarvin@gmail.com>
2024-07-01 00:47:28 +0300

    add router handlers

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	logg "github.com/sirupsen/logrus"
 
+	"github.com/EspiraMarvin/go-crud-postgres/controllers"
 	"github.com/EspiraMarvin/go-crud-postgres/initializers"
 	"github.com/gin-gonic/gin"
 )
@@ -15,10 +16,13 @@ func init() {
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	r.POST("/posts", controllers.PostsCreate)
+	r.GET("/posts", controllers.PostsIndex)
+	r.GET("/posts/:id", controllers.PostsShow)
+	r.PUT("/posts/:id", controllers.PostsUpdate)
+	r.PATCH("/posts/:id", controllers.PostsUpdate) // partial update
+	r.DELETE("/posts/:id", controllers.PostsDelete)
+
 	r.Run() // listen and serve on 0.0.0.0:3000
 }
